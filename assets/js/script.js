109cdc330d5c19c91aa2535fedc1d7b5c7939757
TheHebi <nturcotte8@live.com>
2021-06-30 19:30:11 -0700

    date text area shows current time and updates in 1s intervals

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,14 @@
 // main portion of table is a type field
 // middle changes color based on time of day, passed hour blocks are gray, current hour block is red, upcoming hourblocks are green
 // right portion of table has a save button to save input to system memory
-// TODO: set #currentDay to current time with momentjs
+
+// set #currentDay to current time with momentjs
+var currentTime = function(){
+    $("#currentDay").text(moment().format("dddd MMMM do, YYYY HHmm:ss"))
+}
+setInterval(currentTime, 1000)
 // TODO: change block colors based on current time (if/elseif/else)
+
 // TODO: save input to local storage
 // TODO: click event listener for save btn
 
