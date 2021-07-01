58185d4692c65ffa0e9b830336beac15bc37dcdf
TheHebi <nturcotte8@live.com>
2021-06-30 18:58:28 -0700

    html layout finished and psuedocode with todos in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-console.log("hello")
+// timetable is from 0900 to 1700
+// display current time at top of page
+// left portion of table is timeframe
+// main portion of table is a type field
+// middle changes color based on time of day, passed hour blocks are gray, current hour block is red, upcoming hourblocks are green
+// right portion of table has a save button to save input to system memory
+// TODO: set #currentDay to current time with momentjs
+// TODO: change block colors based on current time (if/elseif/else)
+// TODO: save input to local storage
+// TODO: click event listener for save btn
+
